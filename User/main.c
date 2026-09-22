fc7afbbdd467b67410c6d8d37d2c13e5c88469b2
TX <TX07080926@outlook.com>
2026-09-22 16:05:47 +0800

    可执行的MPU6050读写程序，修改了Main.c文件

--- User/main.c
+++ User/main.c
@@ -1,20 +1,28 @@
 #include "stm32f10x.h"      
 #include "OLED.h"
+#include "MPU6050.h"
 #include "I2C.h"
-
 int main(void)
 {
 	OLED_Init();
-	MI2C_Init();
-	
+	MPU6050_Init();
 	uint8_t ack;
 	OLED_ShowChar(1,1,'a');
 	BaseConfig_Start();
 	BaseConfig_SendByte(0XD0);
 	ack = BaseConfig_ReceiveACK();
 	BaseConfig_Stop();
+	MPU6050_Init();
 
-	OLED_ShowNum(2,1,ack,3);
+	uint8_t data;
+	MPU6050_WriteRegister(0X6B,0X01);
+	MPU6050_WriteRegister(0X19,0XAA);
+	data = MPU6050_ReadRegister(0X19);
+
+	OLED_ShowHexNum(3,1,data,2);
+
+
+	OLED_ShowNum(2,1,ack,2);
 	while(1)
 	{
 
